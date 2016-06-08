05eab451c853c504bf2198b294fc381fdaec6844
Benjamin Melançon <ben@agaric.com>
2016-06-08 11:02:44 -0400

    Update and rename donation entity test

--- tests/src/Kernel/DonationEntityTest.php
+++ tests/src/Kernel/DonationEntityTest.php
@@ -37,30 +37,29 @@ class DonationEntityTest extends EntityKernelTestBase {
     $donation_storage = $this->container->get('entity.manager')->getStorage('give_donation');
     $donation = $donation_storage->create(array('give_form' => 'feedback'));
 
-    // Check for empty values first.
-    $this->assertEqual($donation->getDonation(), '');
-    $this->assertEqual($donation->getSenderName(), '');
-    $this->assertEqual($donation->getSenderMail(), '');
-    $this->assertFalse($donation->copySender());
+   // Check for empty values first.
+    $this->assertEqual($donation->getAmount(), 0);
+    $this->assertEqual($donation->getDonorName(), '');
+    $this->assertEqual($donation->getDonorMail(), '');
+    $this->assertFalse($donation->recurring());
 
     // Check for default values.
-    $this->assertEqual('feedback', $donation->getGiveForm()->id());
-    $this->assertFalse($donation->isPersonal());
+    $this->assertEqual('tzedakah', $donation->getGiveForm()->id());
 
     // Set some values and check for them afterwards.
-    $donation->setDonation('welcome_donation');
-    $donation->setSenderName('sender_name');
-    $donation->setSenderMail('sender_mail');
-    $donation->setCopySender(TRUE);
+    $donation->setAmount(4200);
+    $donation->setDonorName('donor_name');
+    $donation->setDonorMail('donor_mail');
+    $donation->setRecurring(TRUE);
 
-    $this->assertEqual($donation->getDonation(), 'welcome_donation');
-    $this->assertEqual($donation->getSenderName(), 'sender_name');
-    $this->assertEqual($donation->getSenderMail(), 'sender_mail');
-    $this->assertTrue($donation->copySender());
+    $this->assertEqual($donation->getAmount(), 4200);
+    $this->assertEqual($donation->getDonorName(), 'donor_name');
+    $this->assertEqual($donation->getDonorMail(), 'donor_mail');
+    $this->assertTrue($donation->recurring());
 
     $no_access_user = $this->createUser(['uid' => 2]);
-    $access_user = $this->createUser(['uid' => 3], ['access site-wide give form']);
-    $admin = $this->createUser(['uid' => 4], ['administer give forms']);
+    $access_user = $this->createUser(['uid' => 3], ['access give forms']);
+    $admin = $this->createUser(['uid' => 4], ['administer give']);
 
     $this->assertFalse(\Drupal::entityManager()->getAccessControlHandler('give_donation')->createAccess(NULL, $no_access_user));
     $this->assertTrue(\Drupal::entityManager()->getAccessControlHandler('give_donation')->createAccess(NULL, $access_user));
