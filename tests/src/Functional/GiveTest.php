92b14427debcea6db569ff5de48bbf9110a60515
David Valdez <david.valdez210@gmail.com>
2017-02-23 14:43:29 -0600

    Fix the GiveTest test.
    There is still a todo there (and needs more tests, like test the stripe)

--- tests/src/Functional/GiveTest.php
+++ tests/src/Functional/GiveTest.php
@@ -2,10 +2,12 @@
 
 /**
  * @file
- * Contains \Drupal\give\Tests\GiveRecordTest.
+ * Contains \Drupal\give\Tests\GiveTest.
  */
 
 namespace Drupal\Tests\give\Functional;
+
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Tests storing give donations and viewing them through UI.
@@ -15,23 +17,11 @@ namespace Drupal\Tests\give\Functional;
 class GiveRecordTest extends GiveTestBase {
 
   /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  public static $modules = array(
-    'text',
-    'give',
-    'field_ui',
-    'give_test',
-  );
-
-  /**
    * Tests give donations submitted through give form.
    */
   public function testGiveRecord() {
-    // Create and login administrative user.
-    $admin_user = $this->drupalCreateUser(array(
+    // Login administrative user.
+    $this->adminUser = $this->drupalCreateUser(array(
       'access give forms',
       'administer give',
       'create and edit give forms',
@@ -39,67 +29,71 @@ class GiveRecordTest extends GiveTestBase {
       'administer users',
       'administer account settings',
     ));
-    $this->drupalLogin($admin_user);
+
+    $this->drupalLogin($this->adminUser);
     // Create first valid give form.
     $mail = 'simpletest@example.com';
     $this->addGiveForm('test_id', 'test_label', $mail, '', TRUE);
     $this->assertTrue($this->getSession()->getPage()->hasContent('Give form test_label has been added.'));
 
     // Ensure that anonymous users can submit give forms.
-    user_role_grant_permissions(DRUPAL_ANONYMOUS_RID, array('access give forms'));
+    user_role_grant_permissions(AccountInterface::ANONYMOUS_ROLE, ['access give forms']);
     $this->drupalLogout();
     $this->drupalGet('give/test_id');
-    $this->assertText(t('Your email address'));
-    $this->assertNoText(t('Form'));
+    $this->assertSession()->pageTextContains(t('Your email address'));
     $this->submitGive('Test_name', $mail, '22', 'test_id');
 
     // Check to that the user was sent to the step 2.
-    $this->assertTrue(preg_match('/\/give\/test_id\/1/', $this->getSession()->getCurrentUrl()));
+    $this->assertSession()->addressEquals('/give/test_id/1');
+    $this->submitByCheck('12345678', 'Test');
+    $this->assertSession()->pageTextContains('Your donation has been received. Thank you!');
 
     // Login as admin.
-    $this->drupalLogin($admin_user);
-
-    $display_fields = array(
-      "Donor name",
-      "Donor email address",
-      "Amount (USD)"
-    );
+    $this->drupalLogin($this->adminUser);
 
     // Check the donation list overview.
     $this->drupalGet('admin/structure/give/donations');
     $rows = $this->xpath('//tbody/tr');
     // Make sure only 1 donation is available.
-    $this->assertEqual(count($rows), 1);
+    $this->assertEquals(1, count($rows));
     // Some fields should be present.
-    $this->assertText('22.00');
-    $this->assertText('Test_name');
-    $this->assertText('test_label');
+    $this->assertTrue($this->getSession()->getPage()->hasContent('$22.00'));
+    $this->assertTrue($this->getSession()->getPage()->hasContent('Test_name'));
+    $this->assertTrue($this->getSession()->getPage()->hasContent('test_label'));
 
     // Click the view link and make sure name, amount, and email are displayed
     // by default.
-    $this->clickLink(t('View'));
+    $this->clickLink(t('Edit'));
+
+    $display_fields = [
+      "Donor name",
+      "Donor email address",
+      "Amount (USD)",
+    ];
     foreach ($display_fields as $label) {
-      $this->assertText($label);
+      $this->assertSession()->pageTextContains($label);
     }
 
     // Make sure the stored donation is correct.
     $this->drupalGet('admin/structure/give/donations');
     $this->clickLink(t('Edit'));
-    $this->assertFieldById('edit-name', 'Test_name');
-    $this->assertFieldById('edit-mail', $mail);
-    $this->assertFieldById('edit-amount', 22);
+    $this->assertSession()->fieldValueEquals('edit-name', 'Test_name');
+    $this->assertSession()->fieldValueEquals('edit-mail', $mail);
+    $this->assertSession()->fieldValueEquals('edit-amount', 22);
+
+
     // Submit should redirect back to listing.
-    $this->drupalPostForm(NULL, array(), t('Give '));
-    $this->assertUrl('admin/structure/give/donations');
+    $this->drupalPostForm(NULL, [], t('Save'));
+    $this->assertSession()->addressEquals('admin/structure/give/donations');
 
     // Delete the donation.
     $this->clickLink(t('Delete'));
-    $this->drupalPostForm(NULL, NULL, t('Delete'));
+    $this->drupalPostForm(NULL, [], t('Delete'));
     // Make sure no donations are available.
-    $this->assertText('There are no recorded donations yet.');
+    $this->assertSession()->pageTextContains('There are no recorded donations yet.');
 
     // Fill the redirect field and assert the page is successfully redirected.
-    //@todo these tests assume to the donation is made in only one step, update.
+    //@todo Test that the redirect url option is working.
 //    $edit = ['give_record_uri' => 'entity:user/' . $admin_user->id()];
 //    $this->drupalPostForm('admin/structure/give/manage/test_id', $edit, t('Save'));
 //    $edit = [
--- tests/src/Functional/GiveTestBase.php
+++ tests/src/Functional/GiveTestBase.php
@@ -16,6 +16,27 @@ use Drupal\Tests\BrowserTestBase;
 abstract class GiveTestBase extends BrowserTestBase {
 
   /**
+   * Admin User.
+   *
+   * @var \Drupal\user\Entity\User $adminUser
+   */
+  protected $adminUser;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array(
+    'user',
+    'text',
+    'give',
+    'field_ui',
+    'give_test',
+    'views'
+  );
+
+  /**
    * Adds a form.
    *
    * @param string $id
@@ -61,13 +82,24 @@ abstract class GiveTestBase extends BrowserTestBase {
     $edit['name'] = $name;
     $edit['mail'] = $mail;
     $edit['amount'] = $amount;
-    // @todo fix this.
+// @todo check if this is working.
 //    if ($id == $this->config('give.settings')->get('default_form')) {
 //      $this->drupalPostForm('give', $edit, t('Give'));
 //    }
 //    else {
-      $this->drupalPostForm('give/' . $id, $edit, t('Give'));
-    //}
+    $this->drupalPostForm('give/' . $id, $edit, t('Give'));
+  }
+
+  /**
+   * @param string $phone
+   * @param string $check_or_other_information
+   */
+  public function submitByCheck($phone, $check_or_other_information) {
+    $edit = [];
+    $edit['method'] = 3; // By check or other.
+    $edit['telephone'] = $phone;
+    $edit['check_or_other_information'] = $check_or_other_information;
+    $this->drupalPostForm(NULL, $edit, t('Give'));
   }
 
 }
