9a0ac61cd098ce52e5da3d37d2ed61ee8e42bf5b
Benjamin Melançon <ben@agaric.com>
2017-04-23 19:38:56 -0400

    Format source code per Drupal coding standards
    https://www.drupal.org/docs/develop/standards/javascript/javascript-coding-standards

--- js/give-stripe-helper.js
+++ js/give-stripe-helper.js
@@ -7,20 +7,20 @@
     var stripe = Stripe('pk_test_bUh7EpEkPzKqFOptiK9x7TKi'); // settings.give.stripe_publishable_key
     var elements = stripe.elements();
     var card = elements.create('card', {
-        hidePostalCode: true,
-        style: {
-            base: {
-                iconColor: 'red',
-                color: 'green',
-                lineHeight: '2em',
-                fontWeight: 400,
-                fontFamily: '"Helvetica Neue", "Helvetica", sans-serif',
-                fontSize: '15px',
-                '::placeholder': {
-                    color: '#ccc',
-                }
-            },
-        }
+      hidePostalCode: true,
+      style: {
+        base: {
+          iconColor: 'red',
+          color: 'green',
+          lineHeight: '2em',
+          fontWeight: 400,
+          fontFamily: '"Helvetica Neue", "Helvetica", sans-serif',
+          fontSize: '15px',
+          '::placeholder': {
+            color: '#ccc',
+          }
+        },
+      }
     });
     card.mount('#stripe-card-element');
   }
@@ -28,29 +28,28 @@
     alert('This form cannot take credit/debit card payments until the Stripe publishable key is set.');
   }
 
-    function handleResponse(result) {
-      console.log('handling!');
-      var successElement = document.querySelector('#stripe-card-success');
-      var errorElement = document.querySelector('#stripe-card-errors');
-      successElement.classList.remove('visible');
-      errorElement.classList.remove('visible');
+  function handleResponse(result) {
+    var successElement = document.querySelector('#stripe-card-success');
+    var errorElement = document.querySelector('#stripe-card-errors');
+    successElement.classList.remove('visible');
+    errorElement.classList.remove('visible');
 
-      if (result.token) {
-        // Get the token ID:
-        var token = result.token.id;
-        // Insert the token ID into the form so it gets submitted to the server:
-        var form = document.querySelector('.give-donation-form');
-        form.querySelector('input[name=stripe_token]').value = token;
+    if (result.token) {
+      // Get the token ID:
+      var token = result.token.id;
+      // Insert the token ID into the form so it gets submitted to the server:
+      var form = document.querySelector('.give-donation-form');
+      form.querySelector('input[name=stripe_token]').value = token;
 
-        // Submit the form:
-        form.submit();
-        // Use the token to create a charge or a customer
-        // https://stripe.com/docs/charges
-        successElement.querySelector('.token').textContent = result.token.id;
-        successElement.classList.add('visible');
-      } else if (result.error) {
-        errorElement.textContent = result.error.message;
-        errorElement.classList.add('visible');
+      // Submit the form:
+      form.submit();
+      // Use the token to create a charge or a customer
+      // https://stripe.com/docs/charges
+      successElement.querySelector('.token').textContent = result.token.id;
+      successElement.classList.add('visible');
+    } else if (result.error) {
+      errorElement.textContent = result.error.message;
+      errorElement.classList.add('visible');
 /*
         // Show the errors on the form:
         var pre_span = '<div role="contentinfo" aria-label="Error message" class="messages messages--error"><div role="alert">';
@@ -58,26 +57,25 @@
         $form.find('.payment-errors').html(pre_span + response.error.message + post_span);
         $form.find('.submit').prop('disabled', false); // Re-enable submission
 */
-      }
     }
+  }
 
-    card.on('change', function(event) {
-        console.log('telling to handle');
-        handleResponse(event);
-    });
+  card.on('change', function(event) {
+    handleResponse(event);
+  });
 
-    document.querySelector('.give-donation-form').addEventListener('submit', function(e) {
-        // Only try to process the card if card method ('1') is selected.
-        if ($('input[name=method]:checked').val() == 1) {
-          e.preventDefault();
-          var form = document.querySelector('.give-donation-form');
-          var extraDetails = {
-            // name: form.querySelector('input[name=cardholder-name]').value,
-            // address_zip: form.querySelector('input[name=address-zip]').value
-          };
-          stripe.createToken(card, extraDetails).then(handleResponse);
-        }
-    });
+  document.querySelector('.give-donation-form').addEventListener('submit', function(e) {
+    // Only try to process the card if card method ('1') is selected.
+    if ($('input[name=method]:checked').val() == 1) {
+      e.preventDefault();
+      var form = document.querySelector('.give-donation-form');
+      var extraDetails = {
+        // name: form.querySelector('input[name=cardholder-name]').value,
+        // address_zip: form.querySelector('input[name=address-zip]').value
+      };
+      stripe.createToken(card, extraDetails).then(handleResponse);
+    }
+  });
 
 /*
   $form.submit(function(event) {
