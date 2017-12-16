d647e3bb55bcb71c310c34915e62c312bd1485d1
Benjamin Melançon <ben@agaric.com>
2017-12-16 15:00:53 -0500

    First draft of formatter for recurrence field
    Ripped off from User module's UserNameFormatter

---
+++ src/Plugin/Field/FieldFormatter/RecurrenceFormatter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\user\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'user_name' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "give_recurrence",
+ *   label = @Translation("Recurrence"),
+ *   description = @Translation("Display the automatic frequency, if any, with which a donation is given."),
+ *   field_types = {
+ *     "string"
+ *   }
+ * )
+ */
+class UserNameFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+  public static function defaultSettings() {
+    $options = parent::defaultSettings();
+
+    $options['link_to_entity'] = TRUE;
+    return $options;
+  }
+   */
+
+  /**
+   * {@inheritdoc}
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $form['link_to_entity'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Link to the user'),
+      '#default_value' => $this->getSetting('link_to_entity'),
+    ];
+
+    return $form;
+  }
+   */
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      /** @var $donation \Drupal\give\DonationInterface */
+      if ($donation = $item->getEntity()) {
+        $elements[$delta] = [
+          '#markup' => $donation->getRecurrence(),
+          '#cache' => [
+            'tags' => $donation->getCacheTags(),
+          ],
+        ];
+      }
+    }
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(FieldDefinitionInterface $field_definition) {
+    return $field_definition->getTargetEntityTypeId() === 'give_donation' && $field_definition->getName() === 'recurring';
+  }
+
+}
